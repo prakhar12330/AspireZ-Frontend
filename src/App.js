082fab49a94f59ff4d9cb6c24464824e785e6a81
Prakhar Singh <prakhar12330@gmail.com>
2024-04-03 19:09:17 +0530

    Login and signup implemented

--- src/App.js
+++ src/App.js
@@ -1,20 +1,35 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Route, Routes, Navigate, useLocation} from 'react-router-dom';
 import Landing from "./component/Landing";
 import Onboarding from './component/Onboarding';
-import Login from './component/Login';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import { useState } from 'react';
+import Home from './pages/Home';
 
 function App() {
 
+  const [auth, setAuth] = useState(false);
+  const location = useLocation();
   return (
-    <Router>
       <Routes>
         <Route path='/' element={<Landing />} />
         <Route path="/onboarding" element={<Onboarding />} />
-        <Route path="/login" element={<Login />} />
+        <Route path="/login" element={<Login setAuth={setAuth} />} />
+        <Route path="/signup" element={<Signup setAuth={setAuth} />} />
+        <Route
+          path="/"
+          element={
+            auth ? (
+              <Home setAuth={setAuth} />
+            ) : (
+              <Navigate to="/login" state={{ from: location }} replace />
+            )
+          }
+        />
       </Routes>
-    </Router>
+    
   );
 }
 
