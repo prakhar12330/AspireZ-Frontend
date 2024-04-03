426da46acaac697862bd54789c9b5b946fe180c4
Prakhar Singh <prakhar12330@gmail.com>
2024-04-03 13:20:20 +0530

    Landing page updated

--- src/App.js
+++ src/App.js
@@ -3,14 +3,16 @@ import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Landing from "./component/Landing";
 import Onboarding from './component/Onboarding';
+import Login from './component/Login';
 
 function App() {
-  
+
   return (
     <Router>
       <Routes>
-        <Route path='/' element={<Landing/>}/>
-          <Route path="/onboarding" element={<Onboarding/>}/>
+        <Route path='/' element={<Landing />} />
+        <Route path="/onboarding" element={<Onboarding />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </Router>
   );
--- src/component/Landing/index.js
+++ src/component/Landing/index.js
@@ -1,28 +1,44 @@
 import React from 'react'
 import {
-    Box,
+  Box, Button
 } from "@mui/material";
 import aspirezLogo from '../../assets/aspirezLogo.png'
 
 const Landing = () => {
   return (
-    <Box 
-    sx={{
-        bgcolor:'#2A64F6',
-        width:'100%',
-        height:'100vh',
+    <Box
+      sx={{
+        bgcolor: '#2A64F6',
+        width: '100%',
+        height: '100vh',
         overflow: 'hidden',
-        display: 'flex',    
+        display: 'flex',
+        flexDirection: 'column',
         justifyContent: 'center',
         alignItems: 'center',
-    }}
+      }}
     >
-    <Box sx={{
-         textAlign: 'center',
-    }}>
-     <img src={aspirezLogo} height={200} alt={' '}/>
-    </Box>
-
+      <Box sx={{ textAlign: 'center' }}>
+        <img src={aspirezLogo} height={200} alt={' '} />
+      </Box>
+      <Box sx={{ textAlign: 'center', mt: 4 }}>
+        <Button
+          variant="contained"
+          sx={{
+            backgroundColor: '#FFFFFF',
+            '&:hover': {
+              backgroundColor: '#D3D3D3',
+            },
+            padding: 1,
+            width: 200,
+            color: 'black',
+            fontWeight: 'medium',
+          }}
+          href='/onboarding'
+        >
+          Get Started
+        </Button>
+      </Box>
     </Box>
   )
 }
